d631888c1d7171f18a40374742652984967ed55b
syamuelim <u3578305@connect.hku.hk>
2023-09-13 22:08:39 +0800

    init commit of the backend

---
+++ demo/src/main/java/com/fyp/demo/ReputationMonitoringApplication.java
@@ -0,0 +1,13 @@
+package com.fyp.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReputationMonitoringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReputationMonitoringApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/fyp/demo/ReputationMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fyp.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReputationMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
