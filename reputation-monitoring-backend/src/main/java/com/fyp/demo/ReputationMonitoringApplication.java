1f79fcf676077017d50a1ca33d4a7d5bf048d507
syamuelim <u3578305@connect.hku.hk>
2023-09-17 11:10:55 +0800

    housekeeping

--- reputation-monitoring-backend/src/main/java/com/fyp/demo/ReputationMonitoringApplication.java
+++ reputation-monitoring-backend/src/main/java/com/fyp/demo/ReputationMonitoringApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReputationMonitoringApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReputationMonitoringApplication.class, args);
 	}
