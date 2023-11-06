651056167bd7c9eff0261a2fc836bf1bf1a01792
syamuelim <u3578305@connect.hku.hk>
2023-11-06 23:07:36 +0800

    tested few api

--- reputation-monitoring-frontend/AppConfig.js
+++ reputation-monitoring-frontend/AppConfig.js
@@ -1,2 +1,4 @@
 export const baseUrl = "http://localhost:8080/"
+export const facebookUrl = "https://graph.facebook.com/v18.0/"
+export const instagramUrl = ""
 
--- reputation-monitoring-frontend/src/page/FacebookLogin.js
+++ reputation-monitoring-frontend/src/page/FacebookLogin.js
@@ -1,4 +1,4 @@
-import { View, Text, Button, StyleSheet } from "react-native";
+import { View, Text, Button, StyleSheet, Image } from "react-native";
 import React, { useState, useEffect, setState, useRef } from "react";
 import { useNavigation } from "@react-navigation/native";
 
@@ -7,13 +7,17 @@ import * as testService from "../service/Test";
 import * as Facebook from "expo-auth-session/providers/facebook";
 import * as WebBrowser from "expo-web-browser";
 
+import {facebookPermission} from "../Config/Menu";
+
 
 const FacebookLoginScreen = ({ navigation }) => {
   WebBrowser.maybeCompleteAuthSession();
   const [user, setUser] = useState(null);
+
   const [request, response, promptAsync] = Facebook.useAuthRequest({
     clientId: "652157000425074",
-    configId: "3654644834792585",
+    scopes: facebookPermission,
+    configId: "3654644834792585"
   })
   const [students, setStudent] = useState([]);
 
@@ -25,20 +29,16 @@ const FacebookLoginScreen = ({ navigation }) => {
     //loadStudents();
   }, []);
 
-  useEffect(() => {
-    console.log("set", students);
-  }, [students]);
-
 
   useEffect(() => {
     if (response && response.type === "success" && response.authentication) {
       (async () => {
         const userInfoResponse = await fetch(
-          `https://graph.facebook.com/me?access_token=${response.authentication.accessToken}&fields=id,name,picture.type(large)`
+          `https://graph.facebook.com/me?fields=id,name,email,birthday,picture.type(large)&access_token=${response.authentication.accessToken}`
         );
         const userInfo = await userInfoResponse.json();
         setUser(userInfo);
-        console.log(JSON.stringify(response, null, 2));
+        console.log(userInfoResponse);
       })();
     }
   }, [response]);
@@ -49,30 +49,23 @@ const FacebookLoginScreen = ({ navigation }) => {
       return;
     }
   };
+
   //#endregion
 
-  //#region load service
-  let loadStudents = async () => {
-    const response = await testService.getStudents();
-    setStudent(response.data);
-  };
-
-  
-  //#endregion
   return (
     <View style={styles.container}>
       {user ? (
         <Profile user={user} />
-        ) : (
+      ) : (
         <Button
           disabled={!request}
           title="Sign in with Facebook"
           onPress={handlePressAsync}
-          />
+        />
       )}
     </View>
   );
-  
+
 };
 
 const styles = StyleSheet.create({
@@ -95,13 +88,13 @@ const styles = StyleSheet.create({
 });
 
 function Profile({ user }) {
-return (
-  <View style={styles.profile}>
-    <Image source={{ uri: user.picture.data.url }} style={styles.image} />
-    <Text style={styles.name}>{user.name}</Text>
-    <Text>ID: {user.id}</Text>
-  </View>
-);
+  return (
+    <View style={styles.profile}>
+      <Image source={{ uri: user.picture.data.url }} style={styles.image} />
+      <Text style={styles.name}>{user.name}</Text>
+      <Text>ID: {user.id}</Text>
+    </View>
+  );
 }
 
 export default FacebookLoginScreen;
